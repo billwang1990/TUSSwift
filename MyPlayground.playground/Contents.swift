dc34ba9832d9f5e5bac09875abc7526f2996ace1
Yaqing Wang <yaqwang@thoughtworks.com>
2015-11-06 14:04:58 +0800

    :)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+var dic:[String:String] = [:]
+dic.updateValue("val212", forKey: "key")
+print(dic)
+dic.removeValueForKey("as")
+print(dic)
